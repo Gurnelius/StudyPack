f8831ef170d60ca88c60d0b5f8e38dc3dba82f22
Gurnelius Terer <terergurnelius@gmail.com>
2026-03-17 11:23:56 +0300

    Refactor PaymentMethodTest to improve variable visibility and correct test naming

--- src/test/kotlin/PaymentMethodTest.kt
+++ src/test/kotlin/PaymentMethodTest.kt
@@ -9,31 +9,29 @@ class PaymentMethodTest {
     private lateinit var creditCard: PaymentMethod.CreditCard
     private lateinit var mobileMoney: PaymentMethod.MobileMoney
 
-    val phoneNumber = "0728456854"
-    val bankName = "Equity"
-    val accountNumber = "483843943853353"
-    val cardNumber = "43853355383833"
-    val expiryDate = "10/28"
-    val provider = "Safaricom"
-    val amount = 1500.9
+    private var phoneNumber = "0728456854"
+    private var bankName = "Equity"
+    private var accountNumber = "483843943853353"
+    private var cardNumber = "43853355383833"
+    private var expiryDate = "10/28"
+    private var provider = "Safaricom"
+    private var amount = 1500.0
 
     @BeforeEach                          // runs before every test — like setUp() in Python
     fun setUp() {
 
-
-        bankTransfer = PaymentMethod.BankTransfer(bankName, accountNumber)
-        creditCard = PaymentMethod.CreditCard(cardNumber = cardNumber, expiryDate)
-        mobileMoney = PaymentMethod.MobileMoney(phoneNumber, provider)
+        bankTransfer = PaymentMethod.BankTransfer(bankName = bankName, accountNumber = accountNumber)
+        creditCard = PaymentMethod.CreditCard(cardNumber = cardNumber, expiryDate = expiryDate)
+        mobileMoney = PaymentMethod.MobileMoney(phoneNumber = phoneNumber, provider = provider)
     }
 
     @AfterEach                           // runs after every test — like tearDown() in Python
     fun tearDown() {
         println("Test completed, cleaning up...")
-        // close DB connections, clear files, reset state, etc.
     }
 
     @Test
-    fun test_bank_transfer_returns_emergency_message() {
+    fun test_bank_transfer_returns_the_correct_message() {
         val result = processPayment(amount=amount, method=bankTransfer)
         assertEquals(
             "Transferring KES $amount to $accountNumber at $bankName",
